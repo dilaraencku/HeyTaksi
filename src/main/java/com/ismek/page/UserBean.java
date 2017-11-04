d11a1618b1bf5bef1de7c49c8a89246739f3c49b
yahya <yahyacan55@gmail.com>
2017-11-04 14:51:22 +0300

    UserBean değiştirildi.

--- src/main/java/com/ismek/page/UserBean.java
+++ src/main/java/com/ismek/page/UserBean.java
@@ -1,21 +1,41 @@
 package com.ismek.page;
+import java.util.Date;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
 import com.ismek.dao.UserDAO;
 import com.ismek.entity.User;
+
 @ManagedBean(name = "userBean", eager = true)
 @RequestScoped
 public class UserBean {
 	private String name;
-	private Integer age;
+	private Date birthday;
+	private String lastName;
+	private String email;
+	private String password;
+	
 	public String saveUser(){
 		UserDAO userDao = new UserDAO();
 		Integer userId= userDao.getId();
-		User user = new User(userId, name, age);
+		User user = new User(userId, name,lastName,email,birthday,password);
 		userDao.save(user);
 		System.out.println("User successfully saved.");
 		return "output";
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	public String getName() {
 		return name;
@@ -23,10 +43,16 @@ public class UserBean {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Integer getAge() {
-		return age;
+	public Date getBirthday() {
+		return birthday;
 	}
-	public void setAge(Integer age) {
-		this.age = age;
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
 	}
 }
