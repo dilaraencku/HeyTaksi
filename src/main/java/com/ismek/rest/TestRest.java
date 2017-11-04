3136d8c109f9090f10ae6dca7c541508d3b31e9b
yahya <yahyacan55@gmail.com>
2017-11-04 15:13:02 +0300

    Servislere produces consumes eklendi

--- src/main/java/com/ismek/rest/TestRest.java
+++ src/main/java/com/ismek/rest/TestRest.java
@@ -2,8 +2,11 @@ package com.ismek.rest;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import com.ismek.dao.UserDAO;
 import com.ismek.entity.User;
@@ -19,6 +22,8 @@ public class TestRest {
 	
 	@GET
 	@Path("/findAllUser")
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 	public List<User> findAllUser(){
 		return new UserDAO().findAll();
 	}
